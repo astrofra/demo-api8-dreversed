d807a4a43053fefa9bda517e2c1e61458ef43acb
fra <astrofra@gmail.com>
2025-03-26 10:26:19 +0100

    WIP title scene.

--- app/sequences/title_scene.lua
+++ app/sequences/title_scene.lua
@@ -17,7 +17,17 @@ function SetupTitleScene(_scene, _res, _pipeline_info)
     local _flags = hg.LSSF_Nodes | hg.LSSF_Physics
     hg.LoadSceneFromAssets("sequences/title_tv.scn", _scene, _res, _pipeline_info, _flags)
 
-    rb_nodes = {}
+    -- automatically grab the physics nodes
+    local rb_nodes = {}
+    local _nodes = _scene:GetNodes()
+    local i
+    for i = 0, _nodes:size() - 1 do
+        local _node = _nodes:at(i)
+        if _node:HasInstance() == true then
+            local _new_node = _node:GetInstanceSceneView():GetNode(_scene, "root")
+            table.insert(rb_nodes, _new_node)
+        end
+    end
     
     return rb_nodes
 end 
