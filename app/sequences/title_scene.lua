34a6511e0456f534efdd4013b1753ddcfd1e4202
fra <astrofra@gmail.com>
2025-03-26 11:32:10 +0100

    WIP title scene.

--- app/sequences/title_scene.lua
+++ app/sequences/title_scene.lua
@@ -3,17 +3,6 @@ require("physics_utils")
 
 function SetupTitleScene(_scene, _res, _pipeline_info)
     -- specific physics setup
-    -- LSSF_Nodes
-    -- LSSF_Scene
-    -- LSSF_Anims
-    -- LSSF_KeyValues
-    -- LSSF_Physics
-    -- LSSF_Scripts
-    -- LSSF_All
-    -- LSSF_QueueTextureLoads
-    -- LSSF_FreezeMatrixToTransformOnSave
-    -- LSSF_QueueModelLoads
-    -- LSSF_DoNotChangeCurrentCameraIfValid
     local _flags = hg.LSSF_Nodes | hg.LSSF_Physics
     hg.LoadSceneFromAssets("sequences/title_tv.scn", _scene, _res, _pipeline_info, _flags)
 
