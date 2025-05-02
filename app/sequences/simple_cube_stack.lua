1954ae18a2b5caa73b86eac4c08513adcc98397e
fra <astrofra@gmail.com>
2025-05-02 17:42:52 +0200

    WIP greetings.

--- app/sequences/simple_cube_stack.lua
+++ app/sequences/simple_cube_stack.lua
@@ -2,6 +2,20 @@ hg = require("harfang")
 require("physics_utils")
 
 -- SetupSimpleCubeStack(scene, res, {model_size = cube_size, model_ref = cube_ref, materials = {mat_grey}})
+
+local grtz = {
+    "grtz_analogue.scn",
+    "grtz_dbug.scn",
+    "grtz_desire.scn",
+    "grtz_drflopine.scn",
+    "grtz_flush.scn",
+    "grtz_gargaj.scn",
+    "grtz_mankind.scn",
+    "grtz_mooz.scn",
+    "grtz_norecess.scn",
+    "grtz_nusan.scn"
+}
+
 function SetupSimpleCubeStack(_scene, res, pipeline_info, params)
 -- specific physics setup
     local _cube_size, _cube_ref, _generic_material = params.model_size, params.model_ref, params.materials.grey
@@ -9,9 +23,11 @@ function SetupSimpleCubeStack(_scene, res, pipeline_info, params)
     local rb_nodes = {}
     for i = 1, 200 do
         local _new_node, _ = CreatePhysicCubeEx(_scene, _cube_size, hg.TranslationMat4(hg.Vec3(0, 1 + i * 5, 0)), _cube_ref, {_generic_material}, hg.RBT_Dynamic, 1)
-        local _greets, _ = hg.CreateInstanceFromAssets(_scene, hg.TranslationMat4(hg.Vec3(0, 0, 0)), "props/greets/grtz_flush.scn", res, pipeline_info)
-        _greets:GetTransform():SetParent(_new_node)
-        _greets:GetTransform():SetRot(hg.Vec3(0, hg.DegreeToRadian(i * -15.0), 0))
+        if i <= #grtz then
+            local _greets, _ = hg.CreateInstanceFromAssets(_scene, hg.TranslationMat4(hg.Vec3(0, 0, 0)), "props/greets/" .. grtz[i], res, pipeline_info)
+            _greets:GetTransform():SetParent(_new_node)
+            _greets:GetTransform():SetRot(hg.Vec3(0, hg.DegreeToRadian(i * -15.0), 0))
+        end
         table.insert(rb_nodes, _new_node)
     end
 
